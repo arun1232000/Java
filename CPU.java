ff5ab0bf46588c73f14ce1811df3e7c31ecb4c57
arun1232000 <94525520+arun1232000@users.noreply.github.com>
2022-04-01 16:17:35 +0530

    Create CPU.java

---
+++ CPU.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpu;
+
+
+/**
+ *
+ * @author sjcet
+ */
+public class CPU 
+{
+     double price;
+      class Processor{
+     double cores;
+        String manufacturer;
+
+        double getCache(){
+            return 4.3;
+        }
+    }
+      protected class RAM{
+
+        // members of protected nested class
+        double memory;
+        String manufacturer;
+
+        double getClockSpeed(){
+            return 5.5;
+        }
+    }
+
+ 
+
+    /**
+     * @param args the command line arguments
+     */
+public static void main(String[] args) {
+       CPU cpu = new CPU();
+        CPU.Processor processor = cpu.new Processor();
+ CPU.RAM ram = cpu.new RAM();
+        System.out.println("Processor Cache = " + processor.getCache());
+        System.out.println("Ram Clock speed = " + ram.getClockSpeed());
+    }
+}
